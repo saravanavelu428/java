981267eeef4d49440a433bd235d754a4317cdc90
saravanavelu428 <svelu428@gmail.com>
2025-02-21 11:05:02 +0530

    Add files via upload

---
+++ day_2/Ifelseif.java
@@ -0,0 +1,20 @@
+public class Ifelseif {
+    public static void main(String[] args) {
+        int a=29;
+        int b=87;
+        if(a<b){
+            System.out.println("a is greater:"+a);
+        } 
+        else{
+            System.out.println("Both are same:");
+        }
+    }
+}
+
+
+
+    
+    
+
+    
+
---
+++ day_2/simpleif.java
@@ -0,0 +1,9 @@
+public class simpleif{
+    public static void main(String[] args) {
+        int a=50;
+        int b=60;
+        if(a<b){
+            System.out.println("b is greater:"+b);
+        }
+    }
+}
