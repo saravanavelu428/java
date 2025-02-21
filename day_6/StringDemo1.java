6703977f17ee34b92fe7880aa598211bca1d6412
saravanavelu428 <svelu428@gmail.com>
2025-02-21 11:08:54 +0530

    Add files via upload

---
+++ day_6/StringDemo1.java
@@ -0,0 +1,14 @@
+public class StringDemo1 {
+    public static void main(String[] args) {
+        String str="A B C D";
+        String res="";
+        for(int i=str.length()-1;i>=0;i--)
+        {
+            res=res+str.charAt(i);
+
+        }
+        System.out.println(res);
+
+    }
+    
+}
